2760d23678b3ea9a1d27c5989ffa3ed1f6c7edbe
pramothvs <pramothv2@gmail.com>
2024-07-08 01:01:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function myFunction() {
+    document.getElementById("demo").textContent = Math.floor(Math.random()*100)+1;
+  }
+
+function ClearAll(){
+    document.getElementById("demo").textContent = "00";
+}
